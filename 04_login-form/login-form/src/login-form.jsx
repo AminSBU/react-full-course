29a339f02fd522e7c4dee51ba567fd01b45728b2
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-04-19 17:02:04 +0330

    first issue for login form

---
+++ 04_login-form/login-form/src/login-form.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';  
+
+function Login()
+{
+    return (  
+        <div>  
+          <input   
+            type="text"   
+            className="textInput"   
+            placeholder="type something..."   
+          />  
+        </div>  
+      );  
+    } 
+
+export default Login;
