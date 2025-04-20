fe396598c7ad24637be0578fd5ac36ba1dece362
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-04-20 19:34:53 +0330

    use div to seperate two inputs, add password input text

--- 04_login-form/login-form/src/login-form.jsx
+++ 04_login-form/login-form/src/login-form.jsx
@@ -3,13 +3,26 @@ import React, { useState } from 'react';
 function Login()
 {
     return (  
-        <div>
-            <p className='user-text'>Username</p>
-          <input   
-            type="text"   
-            className="textInput"   
-            placeholder="type something..."   
-          />
+        <div className='item-container'>
+          <h2>Login Form</h2>
+          <p className='user-text'>Username</p>
+          <div>
+            <input   
+              type="text"   
+              className="textInput"   
+              placeholder="type something..."   
+            />
+          </div>
+
+          <div>
+            <p className='user-text'>Password</p>
+            <input   
+              type="text"   
+              className="textInput"   
+              placeholder="type something..."  
+              required 
+            />
+          </div>
         </div>  
       );  
     } 
