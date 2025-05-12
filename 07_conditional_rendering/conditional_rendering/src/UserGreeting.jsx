bb90ee740d637813f21b0923183b838eba6fec86
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-05-12 23:29:34 +0330

    use const for welcome and faild message

--- 07_conditional_rendering/conditional_rendering/src/UserGreeting.jsx
+++ 07_conditional_rendering/conditional_rendering/src/UserGreeting.jsx
@@ -1,5 +1,9 @@
 function UserGreeting(props){
-    return(props.isLoggedIn ? <h3>Welcome {props.username}</h3> : <h3>Please login first</h3>);
+    const WelcomeMessage = <h3>Welcome {props.username}</h3>;
+
+    const FaildMessage   = <h3>Please login first</h3>;
+    
+    return(props.isLoggedIn ? WelcomeMessage : FaildMessage);
 }
 
 export default UserGreeting;
