43fa21c56a5c95ccadde6eecbef08195b35be6a6
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-05-12 16:58:25 +0330

    set condtion for props to show information

--- 07_conditional_rendering/conditional_rendering/src/UserGreeting.jsx
+++ 07_conditional_rendering/conditional_rendering/src/UserGreeting.jsx
@@ -1,5 +1,10 @@
-function UserGreeting(){
-
+function UserGreeting(props){
+    if(props.isLoggedIn){
+        return <h3>Welcome {props.username}</h3>
+    }
+    else{
+        return <h3>Please login first</h3>
+    }
 }
 
 export default UserGreeting;
