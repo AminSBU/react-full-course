9bdffb0584724eca58c1521f45dab9fbe5b98799
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-05-12 17:02:56 +0330

    change structure of return

--- 07_conditional_rendering/conditional_rendering/src/UserGreeting.jsx
+++ 07_conditional_rendering/conditional_rendering/src/UserGreeting.jsx
@@ -1,10 +1,5 @@
 function UserGreeting(props){
-    if(props.isLoggedIn){
-        return <h3>Welcome {props.username}</h3>
-    }
-    else{
-        return <h3>Please login first</h3>
-    }
+    return(props.isLoggedIn ? <h3>Welcome {props.username}</h3> : <h3>Please login first</h3>);
 }
 
 export default UserGreeting;
