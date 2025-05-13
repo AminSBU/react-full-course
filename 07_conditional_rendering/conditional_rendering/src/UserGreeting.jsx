491088633adbf9c5758fa5f5593934ed6c015084
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-05-13 22:44:14 +0330

    use proptypes and defaultProps

--- 07_conditional_rendering/conditional_rendering/src/UserGreeting.jsx
+++ 07_conditional_rendering/conditional_rendering/src/UserGreeting.jsx
@@ -1,9 +1,21 @@
+import PropTypes from 'prop-types';
+
 function UserGreeting(props){
     const WelcomeMessage = <h3>Welcome {props.username}</h3>;
 
     const FaildMessage   = <h3>Please login first</h3>;
-    
+
     return(props.isLoggedIn ? WelcomeMessage : FaildMessage);
+}
+
+UserGreeting.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  username: PropTypes.string
+}
+
+UserGreeting.defaultProps = {
+    isLoggedIn: false,
+    username: "Guest"
 }
 
 export default UserGreeting;
