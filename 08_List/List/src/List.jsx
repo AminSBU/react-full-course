08628537f5b12c5f023af7fce68a695e51d1ecd2
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-05-16 23:11:18 +0330

    use key id and name and more information

--- 08_List/List/src/List.jsx
+++ 08_List/List/src/List.jsx
@@ -1,10 +1,13 @@
 
 function List(){
-    const fruits = ["Apple", "Orange", "Banana", "Kiwi"];
+    const fruits = [{id: 1, name: "Apple",  calories: 90}, 
+                    {id: 2, name: "Orange", calories: 50}, 
+                    {id: 3, name: "Banana", calories: 99}, 
+                    {id: 4, name: "Kiwi",   calories: 80}];
 
-    fruits.sort();
-
-    const ListItems = fruits.map(fruits => <li>{fruits}</li>);
+    const ListItems = fruits.map(fruits => <li key={fruits.id}>
+                                                                {fruits.name}: &nbsp;
+                                                                {fruits.calories}</li>);
 
     return(<ol>{ListItems}</ol>);
 }
