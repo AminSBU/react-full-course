fee3faf9db6f5d95451ec096c65fc51ec7de08c2
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-05-18 19:19:08 +0330

    first issue for useState hook

--- 09_click_events/click_events/src/Button.jsx
+++ 09_click_events/click_events/src/Button.jsx
@@ -1,9 +1,7 @@
 
 function Button(){
-    let counter = 0;
-
-    const handleClick = (e) => e.target.textContent = "Clicked";
-
+    const handleClick = (e) => console.log(e);
+    
     return(
             <button onClick={(e) => handleClick(e)}>Click Me</button>
     );
