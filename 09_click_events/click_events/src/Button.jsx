a8aaff282ad804cd7ffc1650b83fa3cdd534e099
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-05-18 19:08:24 +0330

    print events in console

--- 09_click_events/click_events/src/Button.jsx
+++ 09_click_events/click_events/src/Button.jsx
@@ -2,18 +2,10 @@
 function Button(){
     let counter = 0;
 
-    const handleClick = (name) => {
-        if(counter < 3){
-            counter++;
-            console.log(`${name} click me on Button ${counter} times`);
-        }
-        else{
-            console.log(`${name} please dont click on button bro`);
-        }
-    }
+    const handleClick = (e) => console.log(e);
 
     return(
-            <button onClick={() => handleClick("Amin")}>Click Me</button>
+            <button onClick={(e) => handleClick(e)}>Click Me</button>
     );
 }
 export default Button;
