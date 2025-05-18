57613a18fa4b6e6034e5022ac3da0f17b52dee9b
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-05-18 19:49:19 +0330

    set counter by clicking

--- 10_useState_hook/useState_hook/src/MyComponent.jsx
+++ 10_useState_hook/useState_hook/src/MyComponent.jsx
@@ -2,10 +2,15 @@ import React, {useState} from 'react'
 function MyComponent(){
     const [name, setName] = useState("Guest");
     const [age, setAge] = useState(0);
+    const [count, setCounter] = useState(0);
 
     const updateParameters = () => {
         setName("Amin");
         setAge(34);
+    }
+
+    const updateCounter = () => {
+        setCounter(count + 1);
     }
 
     return(
@@ -13,6 +18,9 @@ function MyComponent(){
             <p>Name: {name}</p>
             <p>Age: {age}</p>
             <button onClick={updateParameters}>Set Name</button>
+
+            <p>Counter: {count}</p>
+            <button onClick={updateCounter}>Set Name</button>
         </div>
     );
 }
