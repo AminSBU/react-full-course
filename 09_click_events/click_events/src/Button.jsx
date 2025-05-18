b77a743525187227c53d385695d4d8f64026aba6
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-05-18 18:58:41 +0330

    consider new button to get name and show in cosole

--- 09_click_events/click_events/src/Button.jsx
+++ 09_click_events/click_events/src/Button.jsx
@@ -1,9 +1,13 @@
 
 function Button(){
     const handleClick = () => console.log("click");
+    const handleClick2 = (name) => console.log(`${name} Click on me`);
 
     return(
-        <button onClick={handleClick}>Click Me</button>
+        <>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => handleClick2("Amin")}>Click Me</button>
+        </>
     );
 }
 export default Button;
