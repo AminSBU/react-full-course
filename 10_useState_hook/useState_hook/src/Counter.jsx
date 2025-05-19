32ece271faafe008b7bd7f37ae61c090cd606e84
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-05-19 23:06:57 +0330

    consider counter file

---
+++ 10_useState_hook/useState_hook/src/Counter.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+
+function Counter(){
+    const [count, setCounter] = useState();
+    const [increament, setIncreament] = useState();
+    const [decreament, setDecreament] = useState();
+
+    return(<></>);
+}
+export default Counter;
