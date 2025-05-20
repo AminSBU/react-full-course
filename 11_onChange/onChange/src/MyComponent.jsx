f3543d2f7cf5855079ea804e2b0d1e2e9a5dc356
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-05-20 18:49:07 +0330

    show input text in text

--- 11_onChange/onChange/src/MyComponent.jsx
+++ 11_onChange/onChange/src/MyComponent.jsx
@@ -1,6 +1,19 @@
 import React, {useState} from "react";
 
 function MyComponent(){
+    const [name, setName] = useState("");
 
+    const onChangeHandler = (event) => {
+        setName(event.target.value);
+    }
+
+    return(
+        <>
+            <div>
+                <input value={name} onChange={onChangeHandler}></input>
+                <p>name: {name}</p>
+            </div>
+        </>
+    );
 }
 export default MyComponent;
