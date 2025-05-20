11cd7501f6ec89a00180a90515c4388410fc73a9
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-05-20 19:35:15 +0330

    use textArea to comment

--- 11_onChange/onChange/src/MyComponent.jsx
+++ 11_onChange/onChange/src/MyComponent.jsx
@@ -1,8 +1,9 @@
 import React, {useState} from "react";
 
 function MyComponent(){
-    const [name, setName] = useState("");
-    const [inputValue, setInputValue] = useState("");
+    const [name, setName]               = useState("");
+    const [inputValue, setInputValue]   = useState("");
+    const [comment, setComment]         = useState("");
 
     const onChangeHandler = (event) => {
         setInputValue(event.target.value);
@@ -12,12 +13,18 @@ function MyComponent(){
         setName(inputValue);
     }
 
+    const onCommentHandler = (event) => {
+        setComment(event.target.value);
+    }
+
     return(
         <>
             <div>
                 <input value={inputValue} onChange={onChangeHandler} type="text"></input>
                 <button onClick={onClickHandler}>SHOW</button>
                 <p>Your name: {name}</p>
+                <textarea value={comment} onChange={onCommentHandler} placeholder="type your comment" />
+                <p>Your comment: {comment}</p>
             </div>
         </>
     );
