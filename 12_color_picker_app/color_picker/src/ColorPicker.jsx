9af5d509b9250b89096f8672a8861cc0688d10a3
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-05-21 23:40:55 +0330

    first issue for color picker app

--- 12_color_picker_app/color_picker/src/ColorPicker.jsx
+++ 12_color_picker_app/color_picker/src/ColorPicker.jsx
@@ -1,4 +1,24 @@
-function ColorPicker(){
+import React, {useState} from "react";
 
+function ColorPicker(){
+    const [color, setColor] = useState();
+
+    function onColorHandler(event){
+        setColor(event.target.value);
+    }
+
+    return(
+        <>
+            <div className="color-picker-container">
+                <h2>Color Picker</h2>
+                <div className="color-picker_show">
+                    <p>color selected: {color}</p>
+                </div>
+
+                <label>select a color:</label>
+                <input type="color" value={color} onChange={onColorHandler} />
+            </div>
+        </>
+    );
 }
 export default ColorPicker;
