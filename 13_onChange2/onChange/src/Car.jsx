bae2988b635f865029a7aa359f3cb8db76c1a511
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-05-23 23:20:10 +0330

    consider name and model base on user

--- 13_onChange2/onChange/src/Car.jsx
+++ 13_onChange2/onChange/src/Car.jsx
@@ -5,12 +5,26 @@ function Car(){
                                     model: "AMG",
                                     name: "Mercedes-Benz"});
 
+    function onCarYearHandler(event){
+        setCar({...car, year: event.target.value});
+    }
+
+    function onCarModelHandler(event){
+        setCar({...car, model: event.target.value});
+    }
+
+    function onCarNameHandler(event){
+        setCar({...car, name: event.target.value});
+    }
+
     return(
         <>
             <div>
                 <p>Your favorite car is {car.name}, {car.year}, {car.model}</p>
 
-
+                <input type="number" value={car.year} onChange={onCarYearHandler}></input>
+                <input type="text"   value={car.model}onChange={onCarModelHandler}></input>
+                <input type="text"   value={car.name}onChange={onCarNameHandler}></input>
             </div>
         </>
     );
