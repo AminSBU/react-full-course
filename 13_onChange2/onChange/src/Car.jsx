0c0401aeddcf36ed59c8b722662b1ddad0a621da
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-05-23 23:13:09 +0330

    set default values for show

--- 13_onChange2/onChange/src/Car.jsx
+++ 13_onChange2/onChange/src/Car.jsx
@@ -1,6 +1,18 @@
 import React, {useState} from "react";
 
 function Car(){
+    const [car, setCar] = useState({year: 2025,
+                                    model: "AMG",
+                                    name: "Mercedes-Benz"});
 
+    return(
+        <>
+            <div>
+                <p>Your favorite car is {car.name}, {car.year}, {car.model}</p>
+
+
+            </div>
+        </>
+    );
 }
 export default Car;
