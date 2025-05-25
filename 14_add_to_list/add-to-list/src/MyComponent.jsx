7bf4fea4a874c0bac5e7f829c4a27ad37172adc1
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-05-25 06:01:35 +0330

    map foods in list

--- 14_add_to_list/add-to-list/src/MyComponent.jsx
+++ 14_add_to_list/add-to-list/src/MyComponent.jsx
@@ -1,6 +1,25 @@
 import React, {useState} from "react";
 
 function MyComponent(){
+    const [food, setFood] = useState(["apple", "orange", "strawberry"]);
 
+    function AddToListHandler(){
+
+    }
+
+    function RemoveFromListHandler(){
+
+    }
+
+    return(
+        <>
+            <div>
+                <ul>
+                    <h2>Foods List</h2>
+                    {food.map((food, index) => <li key={index}>{food}</li>)}
+                </ul>
+            </div>
+        </>
+    );
 }
 export default MyComponent;
