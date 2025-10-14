c123ce806220419bd3d5609d884f8bf016cb7353
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-10-14 17:53:20 +0330

    some modifications

--- 16_Create_new_component_class/create_new_component_class/src/App.jsx
+++ 16_Create_new_component_class/create_new_component_class/src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Blog from './Blog.jsx'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
+    </div>
   )
 }
 
