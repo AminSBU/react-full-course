0b42062b63d01b1153f00fe36a15955728ce300b
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-10-15 23:48:35 +0330

    modification

--- 16_Create_new_component_class/create_new_component_class/src/Home.jsx
+++ 16_Create_new_component_class/create_new_component_class/src/Home.jsx
@@ -1,5 +1,13 @@
+import './Home.css'
+
 const Home = () => (
-    <div>Home</div>
+    <>
+        <div>
+            <h1 className="name-portfolio">My Name is</h1>
+            <h1 className="name-portfolio">Amin Bakhshoodeh</h1>
+            <h1 className='name-portfolio'>Embedded Software Developer</h1>
+        </div>
+    </>
 )
 
 export default Home;
