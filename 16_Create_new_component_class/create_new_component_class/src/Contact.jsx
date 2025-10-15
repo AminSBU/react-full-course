d9942461684c9e41268cba2794a937ef9f988369
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-10-15 19:09:39 +0330

    three input with send button in contact page

--- 16_Create_new_component_class/create_new_component_class/src/Contact.jsx
+++ 16_Create_new_component_class/create_new_component_class/src/Contact.jsx
@@ -1,5 +1,20 @@
 const Contact = () => (
-    <div>Contact</div>
+    <div className="contact-container">
+        <form>
+            <div className="txtbox">
+                <input type="text" placeholder="your name: ..."></input>
+            </div>
+            <div className="txtbox">
+                <input type="text" placeholder="your email: ..."></input>
+            </div>
+            <div className="txtbox">
+                <input type="text" placeholder="your Message: ..."></input>
+            </div>
+            <div className="txtbox">
+                <button className="btn">send</button>
+            </div>
+        </form>
+    </div>
 )
 
 export default Contact;
