eca3353fbaef4d35c080543b164cf6fe6b9044e5
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-10-15 19:35:10 +0330

    some modification

--- 16_Create_new_component_class/create_new_component_class/src/Contact.jsx
+++ 16_Create_new_component_class/create_new_component_class/src/Contact.jsx
@@ -1,17 +1,25 @@
+import './Contact.css'
+
 const Contact = () => (
     <div className="contact-container">
         <form>
             <div className="txtbox">
-                <input type="text" placeholder="your name: ..."></input>
+                <label htmlFor="name">Your name: </label>
+                <input type="text" id='name' placeholder="your name: ..."></input>
             </div>
             <div className="txtbox">
-                <input type="text" placeholder="your email: ..."></input>
+                <label htmlFor="email">Your email: </label>
+                <input type="text" id='email' placeholder="your email: ..."></input>
             </div>
             <div className="txtbox">
-                <input type="text" placeholder="your Message: ..."></input>
+                <label htmlFor="message">Your message: </label>
+            </div>
+            <div className="txtbox">
+                <textarea id='message'  placeholder="your Message: ..." row={10} cols={50}></textarea >
             </div>
             <div className="txtbox">
                 <button className="btn">send</button>
+                <button className="btn">clear</button>
             </div>
         </form>
     </div>
