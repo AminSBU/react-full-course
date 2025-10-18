b731f710f173aa2f553cfc744019f97188ebe0e7
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-10-18 19:24:04 +0330

    useState and button to load posts

--- 16_Create_new_component_class/create_new_component_class/src/Blog.jsx
+++ 16_Create_new_component_class/create_new_component_class/src/Blog.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import POSTS from './POSTS.json'
 import './Blog.css'
 
-const Blog = () => (
-    <div className="post-contents">
-        <ul>
-            {POSTS.map((post) => (
-                <li key={post.id}>
-                <h3>{post.title}</h3>
-                <p>{post.contents}</p>
-                </li>
-            ))}
-        </ul>
-    </div>
-);
+const Blog = () => {
+    const [posts, setPosts] = useState([])
+    return(
+        <div className="post-contents">
+            <button>Load Posts</button>
+            {posts.length === 0 && <div>Empty</div>}
+            <ul>
+                {POSTS.map((post) => (
+                    <li key={post.id}>
+                    <h3>{post.title}</h3>
+                    <p>{post.contents}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default Blog;
