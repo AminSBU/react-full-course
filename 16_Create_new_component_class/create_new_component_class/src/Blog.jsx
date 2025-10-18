dbd663adee3def29200cbe00328282652e7e31a4
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-10-18 19:17:10 +0330

    post contents in  Blog page

--- 16_Create_new_component_class/create_new_component_class/src/Blog.jsx
+++ 16_Create_new_component_class/create_new_component_class/src/Blog.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import POSTS from './POSTS.json'
+import './Blog.css'
 
 const Blog = () => (
-    <div>Blog</div>
-)
+    <div className="post-contents">
+        <ul>
+            {POSTS.map((post) => (
+                <li key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.contents}</p>
+                </li>
+            ))}
+        </ul>
+    </div>
+);
 
 export default Blog;
