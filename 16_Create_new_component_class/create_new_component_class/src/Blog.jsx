497b6919ebce6051e653a801e885b7ac2a9c50a1
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-10-19 17:22:14 +0330

    use loaded for load posts in useEffect

--- 16_Create_new_component_class/create_new_component_class/src/Blog.jsx
+++ 16_Create_new_component_class/create_new_component_class/src/Blog.jsx
@@ -5,9 +5,11 @@ import './Blog.css'
 const Blog = () => {
     const [posts, setPosts] = useState([])
     const [loading, setLoading] = useState(false)
+    const [loaded, setLoaded] = useState(false)
 
     const handleLoad = () => {
         setLoading(true)
+        //setLoaded(true)
         setTimeout(() => {
             setPosts(POSTS);
             setLoading(false)
@@ -17,12 +19,12 @@ const Blog = () => {
     useEffect(() => {
         console.log('useEffect');
         handleLoad();
-    }, []); // تنها یک‌بار اجرا می‌شود
+    }, [loaded]);
 
     return(
         <div className="post-contents">
             <button onClick={handleLoad}>Load Posts</button>
-            {loading ? 'Loading ...' : 'Completed.'}
+            {/* {loading ? 'Loading ...' : 'Completed.'} */}
             {posts.length === 0 && <div>Empty</div>}
             {posts.length > 0 && (
             <ul>
