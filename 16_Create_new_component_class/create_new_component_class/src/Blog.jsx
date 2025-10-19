18c34166b9dbf2eb964fdb593942a2155716716c
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-10-19 17:48:08 +0330

    show three of posts with slice

--- 16_Create_new_component_class/create_new_component_class/src/Blog.jsx
+++ 16_Create_new_component_class/create_new_component_class/src/Blog.jsx
@@ -34,7 +34,7 @@ const Blog = () => {
 
       {!loading && posts.length > 0 && (
         <ul>
-          {posts.map((post) => (
+          {posts.slice(0,3).map((post) => (
             <li key={post.id}>
               <h3>{post.title}</h3>
               <p>{post.body}</p>
